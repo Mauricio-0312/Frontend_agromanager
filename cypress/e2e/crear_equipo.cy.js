abc6b9988b687c0eae35cc06d3f9c4321c550bbc
mauricio <mfragachan11@gmail.com>
2025-11-07 00:10:05 -0400

    Cypress configured

---
+++ cypress/e2e/crear_equipo.cy.js
@@ -0,0 +1,18 @@
+import { slowCypressDown } from 'cypress-slow-down';
+
+import 'cypress-slow-down/commands';
+slowCypressDown(500);
+
+it('crear_equipo', function() {
+     cy.visit('http://localhost:5173/projects')
+     cy.get('#root input[placeholder="email@dominio.com"]').click();
+     cy.get('#root input[placeholder="email@dominio.com"]').type('admin@agro.local');
+     cy.get('#root input[type="password"]').click();
+     cy.get('#root input[type="password"]').type('admin123');
+     cy.get('#root button.rounded').click();
+     cy.get('#root a[href="/equipos"]').click();
+     cy.get('#root [name="descripcion"]').click();
+     cy.get('#root [name="descripcion"]').type('Hacha');
+     cy.get('#root button.bg-primary').click();
+     
+});
